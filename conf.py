815dbeb50f1307668ea88f9da5b29a12c473ecb3
avcopan <avcopan@gmail.com>
2020-05-19 15:01:19 -0400

    Builds page

--- conf.py
+++ conf.py
@@ -13,7 +13,13 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./repos/autofile'))
+sys.path.insert(0, os.path.abspath('./repos/automol'))
 sys.path.insert(0, os.path.abspath('./repos/autoparse'))
+sys.path.insert(0, os.path.abspath('./repos/elstruct'))
+sys.path.insert(0, os.path.abspath('./repos/interfaces'))
+sys.path.insert(0, os.path.abspath('./repos/MESS'))
+sys.path.insert(0, os.path.abspath('./repos/moldriver'))
 
 
 # -- Project information -----------------------------------------------------
